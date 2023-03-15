26494e6f9a2b36736fc09ffd9b9c9fb87feec310
Shah Zeb <zeb00002@algonquinlive.com>
2023-03-15 19:41:01 -0400

    Header Done

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './header.css'
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="gpt__header section__padding" id='home'>
+      <div className="gpt__header-content">
+        <h1 className="gradient__text">
+          Let's build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p> Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <div className="gpt__header-content__input">
+          <input type="email" placeholder='Your Email address' />
+          <button type='button'>Get Started</button>
+        </div>
+        <div className="gpt__header-content__people">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access in last 24 hours</p>
+        </div>
+        
+      </div>
+      <div className="gpt__header-img">
+          <img src={ai} alt="ai" />
+      </div>
+    </div>
   )
 }
 
