6c8ea111fbefad1b724d6a84f62334314bae6bd7
Shah Zeb <zeb00002@algonquinlive.com>
2023-03-21 22:10:30 -0400

    Features done

--- src/containers/whatGPT/WhatGPT.jsx
+++ src/containers/whatGPT/WhatGPT.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Feature } from '../../components/main'
 import './whatgpt.css'
 
 const WhatGPT = () => {
   return (
-    <div>WhatGPT</div>
+    <div id='wgpt' className="gpt__whatgpt section__margin">
+      <div className="gpt__whatgpt-feature">
+        <Feature title= "What is GPT-3" text = "We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+      </div>
+      <div className="gpt__whatgpt-heading">
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt__whatgpt-container">
+        <Feature title="Chatbots" text = "We so opinion friends me message as delight. Whole front do of plate heard oh ought. " />
+        <Feature title="Knowledgebase" text = "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+        <Feature title ="Education" text ="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      </div>
+    </div>
   )
 }
 
