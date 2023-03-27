8c5887c0d0028688371e1b8b5729ec9c20f72b4e
shahzayb <zeb00002@algonquinlive.com>
2023-03-27 02:23:50 -0400

    cta done

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -3,7 +3,15 @@ import './cta.css'
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <div className="gpt__cta">
+      <div className="gpt__cta-content">
+        <p>Request Early Access</p>
+        <h3>Register Today to get early access to different features</h3>
+      </div>
+      <div className="gpt__cta-btn">
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
