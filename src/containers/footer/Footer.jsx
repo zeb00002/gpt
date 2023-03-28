54de2a24199a01853eec70d6fbe7e7f7402932c1
shahzayb <zeb00002@algonquinlive.com>
2023-03-28 16:36:00 -0400

    static website completed

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './footer.css'
+import gpt3Logo from '../../assets/logo.svg';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="gpt__footer section__padding">
+      <div className="gpt__footer-heading">
+        <h1 className='gradient__text'>Do you want to step in to the future before others</h1>
+      </div>
+      <div className="gpt__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt__footer-links">
+        <div className="gpt__footer-links_logo">
+          <img src={gpt3Logo} alt="logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className="gpt__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contract</p>
+        
+        </div>
+        <div className="gpt__footer-links_div">
+          <h4>Get In Touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+          <p>085-1234</p>
+          <p>info@payme.net</p>
+        
+        </div>
+      </div>
+      <div className="gpt__footer-copyright">
+        <p>2023 GPT-3 All Rights Reserved</p>
+      </div>
+    </div>
   )
 }
 
